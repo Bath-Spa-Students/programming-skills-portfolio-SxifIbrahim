3e6ab779acf4ae327aec0424252b955ac9ae5672
SxifIbrahim <sibrahim.rtei@gmail.com>
2023-09-27 13:41:10 +0400

    Exercise 4 and 5

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,5 @@
+Lastname = "Ibrahim"
+Firstname = "Seif"
+Agenumber = "18 years old"
+
+print ("Hi! My name is" + " " + Firstname + " " + Lastname + ", i am " + Agenumber)
---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,11 @@
+# the Value of pi
+pi = 3.14
+
+# the imput from the user
+r = float(input("Input the radius of the circle : "))
+
+# Compute the area from radius of a circle given by the user
+calculatearea = str(pi * r**2);
+
+# Print
+print ("The are of the circle with radius " + str(r) + "is: " + calculatearea)
