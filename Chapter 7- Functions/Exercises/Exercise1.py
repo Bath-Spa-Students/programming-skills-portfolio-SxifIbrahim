fc56be98df52e9ec7d275f1b5b064957ce9adfc6
SxifIbrahim <sibrahim.rtei@gmail.com>
2023-10-20 15:36:16 +0400

    Chapter 7

--- Chapter 7- Functions/Exercises/Exercise1.py
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -1,4 +1,9 @@
-## Exercise 1: Message
+## Exercise 1:   Message  :ballot_box_with_check:
+
+"""
+Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.
+"""
 
 def display_message():
     print("In this chapter, I'm learning about functions in Python.")
