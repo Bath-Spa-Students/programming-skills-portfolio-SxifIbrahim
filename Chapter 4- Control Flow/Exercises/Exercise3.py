338dfa1d700ca26f65a3733552883bedaf635af1
SxifIbrahim <sibrahim.rtei@gmail.com>
2023-10-20 15:35:43 +0400

    Chapter 4

--- Chapter 4- Control Flow/Exercises/Exercise3.py
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -1,4 +1,13 @@
-## Exercise 3: Alien Colors #3
+## Exercise 3: Alien Colors #3 :ballot_box_with_check:
+
+"""
+Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+
+•If the alien is green, print a message that the player earned 5 points.
+•If the alien is yellow, print a message that the player earned 10 points.
+•If the alien is red, print a message that the player earned 15 points.
+•Write three versions of this program, making sure each message is printed for the appropriate color alien.
+"""
 
 alien_color = 'green'
 
