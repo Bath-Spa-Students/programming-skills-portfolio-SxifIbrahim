3c5ba87eb924055ffbcc29e63bc0ac64469a8113
SxifIbrahim <sibrahim.rtei@gmail.com>
2023-10-20 15:35:07 +0400

    Chapter 2

--- Chapter 2- Variables & Comments/Exercises/Exercise1.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -1,4 +1,11 @@
-## Exercise 1: Variables
+## Exercise 1: Variables:ballot_box_with_check:
+
+
+""" 
+Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message. 
+"""
 
 msg = "I love to eat broccoli"
 print (msg)
--- Chapter 2- Variables & Comments/Exercises/Exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -1,4 +1,10 @@
-## Exercise 2: Variables
+## Exercise 2: Variables :ballot_box_with_check:
+
+""" 
+Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new. 
+"""
 
 print ('Neil Armstrong once said, "Thats one small step for a man,')
 print ('a giant leap for mankind."')
